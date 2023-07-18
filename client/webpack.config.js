e0d38874d214a1254d7dae4800da609a72036c49
Cynthia Mory <cynthia.mory.morales@gmail.com>
2023-07-17 20:51:43 -0400

    configured webpack file to run build to get dist.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,12 +19,65 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+
+      // WEBPACK PLUGINS TO GENERATE HTML FILE AND INJECT BUNDLES
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Text Editor",
+      }),
+
+      //INJECT CUSTOM SERVICE WORKER
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+      //MANIFEST.JSON FILE TO CONFIGURE PWA 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Text Editor",
+        short_name: "Text",
+        description: "Keep you notes here!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
+    // CSS LOADER -> TO LOAD CSS FILES AND STYLE LOADER -> TO INJECT CSS INTO THE DOM
     module: {
+
+      // CSS LOADERS AND BABEL TO WEBPACK
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+
+          // BABEL-LOADER -> IN ORDER TO USE ES6
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
