4fe309926ea2d15dfec7553f684003b7606cd099
Cynthia Mory <cynthia.mory.morales@gmail.com>
2023-07-18 05:35:05 -0400

    Updated databases.js & install.js in client folder

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,4 @@
+//IMPORT THE IDB PACKAGE AND CREATE A CONNECTION TO THE DATABASE
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -13,9 +14,40 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// LOGIC FOR A METHOD THAT ADDS CONTENT TO THE DATABASE
+export const putDb = async (content) => {
+  console.log("Update the database");
+
+  //THIS IS TO CREATE A CONNECTION TO THE DATABASE FOR THE VERSION WE WANT TO USE
+  const textDb = await openDB("jate", 1);
+  //THE IS A NEW TRANSACTION AND SPECIFY THE DATABASE AND DATA PRIVILEGES
+  const tx = textDb.transaction("jate", "readwrite");
+  // THIS IS TO OPEN UP THE DESIRED OBJECT STORE
+  const store = tx.objectStore("jate");
+  //THE .PUT() METHOD IS USED ON THE STORE AND CONTENT PASSED IN
+  const request = store.put({ id: 1, value: content });
+  // RESULT IS THE RETURNED VALUE FROM THE REQUEST
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// LOGIC FOR A METHOD THAT GETS ALL THE CONTENT FROM THE DATABASE
+export const getDb = async () => {
+  console.log("GET from the database");
+
+  //A CONNECTION TO THE DATABASE FOR THE VERSION WE WANT TO USE
+  const textDb = await openDB("jate", 1);
+  //THIS IS A NEW TRANSACTION AND SPECIFY THE DATABASE AND DATA PRIVILEGES
+  const tx = textDb.transaction("jate", "readonly");
+  // THIS IS TO OPEN UP THE DESIRED OBJECT STORE
+  const store = tx.objectStore("jate");
+  // THE REQUEST IS THE RETURNED VALUE FROM THE STORE USING THE .GET() METHOD
+  const request = store.get(1);
+  //REQUEST CONFIRMS THE DATA IS RETURNED
+  const result = await request;
+  console.log("result.value", result);
+  return result?.value;
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,31 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    //STORES THE EVENTS TRIGGERED
+    window.deferredPrompt = event;
+    // REMOVES THE HIDDEN CLASS FROM THE BUTTON
+    butInstall.classList.toggle("hidden", false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    //CHECKS IF THE EVENT IS TRIGGERED
+    if (!promptEvent) {
+        return;
+    }
+
+    //SHOWS THE PROMPT
+    promptEvent.prompt();
+    //RESETS THE DEFERRED PROMPT VARIABLE. WHICH CAN ONLY BE USED ONCE
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle("hidden", true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // CLEAR THE PROMPT
+    window.deferredPrompt = null;
+});
